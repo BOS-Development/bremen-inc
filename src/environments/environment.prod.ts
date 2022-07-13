9598a86d9c60d75890223531957485da7b650791
James Lawrence <james@shutupandshave.com>
2022-07-13 21:44:45 +0100

    Added Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  returnURL: 'http%3A//bremen-inc.s3-website.eu-west-2.amazonaws.com/',
+  returnURL: 'https%3A//bremen-inc.web.app/',
   client_id: '421bf932795f4c45a65b49280ddf6498&state=123'
 };
